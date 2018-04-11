a60fdc3629cca3a99380a7f339673c6930d088ca
Levi Lemke <18.levi.lemke@ksd.org>
2018-04-11 12:50:26 -0700

    working on interactions.ll

---
+++ 3DGame/Assets/Levi/interactable.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactable : MonoBehaviour {
+
+    public float radius = 3f;
+    bool isFocus = false;
+    Transform player;
+    bool hasInteracted = false;
+    public Transform interactionTransform;
+    public virtual void Interact()
+    {
+        //this method is meant to be overridden.
+        Debug.Log("interacting with" + transform.name);
+    }
+
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDeFocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (interactionTransform == null)
+        {
+            interactionTransform = transform;
+        }
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+
+    }
+
+}
---
+++ 3DGame/Assets/Levi/pickupObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupObjects : MonoBehaviour {
+    bool iscarryable = false;
+    bool isgame = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("submit"))
+        { //activate or pickup object
+            if (iscarryable)
+            {
+                Destroy(gameObject);  
+            }
+            else if(isgame)
+            {
+                //load scene for the game being interacted with.
+            }
+           
+                    }
+
+        }
+}
