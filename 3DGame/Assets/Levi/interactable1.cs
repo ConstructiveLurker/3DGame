adca1eafe64d53ac2620358c96f4baf6cadc8f10
Levi Lemke <18.levi.lemke@ksd.org>
2018-04-12 13:11:17 -0700

    interaction amost done.ll

---
+++ 3DGame/Assets/Levi/interactable1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactable1 : MonoBehaviour {
+    public float radius = 3f;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, radius);
+}
+
+}
---
+++ 3DGame/Assets/Levi/raycastForInteract.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycastForInteract : MonoBehaviour {
+    public Camera cam;
+    public float range = 3f;
+    void Update()
+    {
+         if(Input.GetButtonDown("select"))
+        {
+            Debug.Log("keypress O");
+            Shoot();
+        }
+       
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward,out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+   
+                }
+}
