2df2b4dea6094c885d3334390503a1fe64932b07
Levi Lemke <18.levi.lemke@ksd.org>
2018-04-26 13:45:24 -0700

    save.ll

---
+++ 3DGame/Assets/Levi/scripts/save.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class save : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        Load();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButton("Save"));
+        Save();
+        Debug.Log("saved");
+    }
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/" + gameObject.name+"Arcade.dat");
+        Save info = new Save();
+        info.x = transform.position.x;
+        info.y = transform.position.y;
+        info.z = transform.position.z;
+        bf.Serialize(file, info);
+        file.Close();
+    }
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/" + gameObject.name+ "Arcade.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/" + gameObject.name +"Arcade.dat", FileMode.Open);
+            Save info = (Save)bf.Deserialize(file);
+            file.Close();
+            transform.position = new Vector3(info.x, info.y, info.z);
+
+        }
+    }
+
+}
+    [Serializable]
+        public class Save
+        {
+        public float x;
+        public float y;
+        public float z;
+        }
+
