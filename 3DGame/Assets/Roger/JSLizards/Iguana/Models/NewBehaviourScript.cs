53caf9f8b5d77af0e30d68c3a7ecaf94beb84fe5
Roger Hays <19.roger.hays@ksd.org>
2018-04-27 13:38:34 -0700

    hlep

---
+++ 3DGame/Assets/Roger/JSLizards/Iguana/Models/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) 
+		{
+			anim.SetInteger ("AnimState", 0);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) 
+		{
+			anim.SetInteger ("AnimState", 1);
+		}
+	}
+}
