e1a35b56301a97e11cc93b8f1f0df32677d28907
Talia Avery <18.talia.avery@ksd.org>
2018-05-02 13:30:19 -0700

    UI and maze stuff

---
+++ 3DGame/Assets/Tyler/Scripts/MazeMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MazeMenu : MonoBehaviour 
+{
+	public void Easy ()
+	{
+		SceneManager.LoadScene ("Sandbox");
+
+		PlayerPrefs.SetInt ("Length", 18);
+		PlayerPrefs.SetInt ("Width", 12);
+
+		PlayerPrefs.SetString ("Difficulty", "Easy");
+	}
+
+	public void Normal ()
+	{
+		SceneManager.LoadScene ("Sandbox");
+
+		PlayerPrefs.SetInt ("Length", 22);
+		PlayerPrefs.SetInt ("Width", 18);
+
+		PlayerPrefs.SetString ("Difficulty", "Normal");
+	}
+
+	public void Hard ()
+	{
+		SceneManager.LoadScene ("Sandbox");
+
+		PlayerPrefs.SetInt ("Length", 26);
+		PlayerPrefs.SetInt ("Width", 22);
+
+		PlayerPrefs.SetString ("Difficulty", "Hard");
+	}
+}
---
+++ 3DGame/Assets/Tyler/Scripts/MazeWinLocation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MazeWinLocation : MonoBehaviour 
+{
+	void Start () 
+	{
+		if (PlayerPrefs.GetString ("Difficulty") == "Easy") 
+		{
+			Vector3 position = new Vector3 (60.0f, 0.0f, 96.0f);
+			gameObject.transform.position = position;
+		}
+
+		if (PlayerPrefs.GetString ("Difficulty") == "Normal") 
+		{
+			Vector3 position = new Vector3 (102.0f, 0.0f, 126.0f);
+			gameObject.transform.position = position;
+		}
+
+		if (PlayerPrefs.GetString ("Difficulty") == "Hard") 
+		{
+			Vector3 position = new Vector3 (114.0f, 0.0f, 150.0f);
+			gameObject.transform.position = position;
+		}
+
+	}
+		
+	void OnTriggerEnter (Collider collision)
+	{
+		if (collision.gameObject.name == "FPSController") 
+		{
+			SceneManager.LoadScene ("UI");
+		}
+	}
+}
